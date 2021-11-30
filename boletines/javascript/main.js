a96aad48efbe80b0d9e1e40dc89b7e0dcd121351
Enrique Ginnari <90937580+enriqueginnari09@users.noreply.github.com>
2021-11-30 09:47:03 -0400

    redireccion /boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -29,7 +29,7 @@ links.forEach(function(link){
        content.classList.add("animated")
        
        setTimeout(function(){
-          location.href = "/";
+          location.href = "/boletines";
        },600);
        
 
