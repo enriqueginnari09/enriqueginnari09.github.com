fb481074fd63996b6c87797f871f095133d764db
Enrique Ginnari <90937580+enriqueginnari09@users.noreply.github.com>
2021-11-30 09:43:50 -0400

    Añadimos la pagina boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,42 @@
+//DOM
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+  console.log(link);
+});
+*/
+
+// obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+// Recorrelos
+
+links.forEach(function(link){
+
+    // Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+       ev.preventDefault();
+       let content = document.querySelector('.content');
+
+       // Quitar las clases de animacion que ya tiene
+       content.classList.remove("fadeInDown")
+       content.classList.remove("animated")
+       
+       // Agregar clases para animar su salida fadeOutUp
+       content.classList.add("fadeOutUp")
+       content.classList.add("animated")
+       
+       setTimeout(function(){
+          location.href = "/";
+       },600);
+       
+
+       return false;
+    });
+});
+
+
+
+
